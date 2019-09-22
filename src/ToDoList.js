384b0caae7cba727a35e456907237323c9e06940
Lucas_Yi <423188112@qq.com>
2019-09-22 20:52:44 +1000

    seperate UI and logic

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css'
-import { Input , Button , List } from 'antd'
 import store from './store/index'
 import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators'
+import ToDoListUi from './ToDoListUi'
 
 
 class TodoList extends Component {
@@ -12,32 +11,19 @@ class TodoList extends Component {
         this.changeInputValue = this.changeInputValue.bind(this)
         this.storeChange = this.storeChange.bind(this)
         this.addClick = this.addClick.bind(this)
+        this.deleteClick = this.deleteClick.bind(this)
         store.subscribe(this.storeChange)
     }
 
     render() { 
         return ( 
-            <div style={{margin:'10px'}}>
-                <div>
-                    <Input 
-                        placeholder={this.state.inputValue} 
-                        value={this.state.inputValue}
-                        style={{ width:'250px', marginRight:'10px'}}
-                        onChange = {this.changeInputValue}    
-                    />
-                    <Button 
-                        type="primary"
-                        onClick = {this.addClick}
-                    >增加</Button>
-                </div>
-                <div style={{margin:'10px',width:'300px'}}>
-                    <List
-                        bordered
-                        dataSource={this.state.list}
-                        renderItem={(item,index)=>(<List.Item onClick={this.deleteClick.bind(this, index)}>{item}</List.Item>)}
-                    />    
-                </div>
-            </div>
+            <ToDoListUi 
+                inputValue={this.state.inputValue}
+                list={this.state.list}
+                changeInputValue={this.changeInputValue}
+                addClick={this.addClick}
+                deleteClick={this.deleteClick}
+            />
          );
     }
 
